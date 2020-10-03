1c874ee3df1aa78d32754f16dc6e31310ec50e61
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-10-03 16:35:07 -0400

    player movement

--- Player.js
+++ Player.js
@@ -5,6 +5,10 @@ export default class Player {
     this._w = w;
     this._h = h;
     this._c = c;
+    this.movement = { UP: false, DOWN: false, LEFT: false, RIGHT: false }
+    this.vx = 0;
+    this.vy = 0;
+    this.acceleration = 2;
   }
 
   get c() {
@@ -25,7 +29,14 @@ export default class Player {
   }
 
   update() {
-    // TODO...
+    if (this.movement.UP && !this.movement.DOWN) this.vy -= this.acceleration;
+    if (this.movement.DOWN && !this.movement.UP) this.vy += this.acceleration;
+    if (this.movement.LEFT && !this.movement.RIGHT) this.vx -= this.acceleration;
+    if (this.movement.RIGHT && !this.movement.LEFT) this.vx += this.acceleration;
+    if (!this.movement.DOWN && !this.movement.UP) this.vy = 0;
+    if (!this.movement.LEFT && !this.movement.RIGHT) this.vx = 0;
+    this.x += this.vx;
+    this.y += this.vy
   }
 
 }
