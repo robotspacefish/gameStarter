9d983ab281d1f8be32fb7e914fd27cce635f28ee
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-10-03 17:15:08 -0400

    move loop into Game class

--- main.js
+++ main.js
@@ -1,33 +1,20 @@
-import Player from './Player.js';
+import Game from './Game.js';
 
-const player = new Player(10, 10);
-const ctx = document.getElementById('canvas').getContext('2d');
-let RAF;
-
-function loop() {
-  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-  player.draw(ctx);
-  player.update();
-  RAF = requestAnimationFrame(loop);
-}
+const game = new Game(document.getElementById('canvas').getContext('2d'));
 
 function keyHandler(e) {
   const keyState = e.type === 'keydown';
-  console.log(e.keyCode)
   // Up upArrow / W / Z
-  if (e.keyCode == 38 || e.keyCode == 90 || e.keyCode == 87) player.movement.UP = keyState;
+  if (e.keyCode == 38 || e.keyCode == 90 || e.keyCode == 87) game.player.movement.UP = keyState;
   // Right (rightArrow / D)
-  if (e.keyCode == 39 || e.keyCode == 68) player.movement.RIGHT = keyState;
+  if (e.keyCode == 39 || e.keyCode == 68) game.player.movement.RIGHT = keyState;
   // Down (downArrow / S)
-  if (e.keyCode == 40 || e.keyCode == 83) player.movement.DOWN = keyState;
+  if (e.keyCode == 40 || e.keyCode == 83) game.player.movement.DOWN = keyState;
   // Left (leftArrow / A / Q)
-  if (e.keyCode == 37 || e.keyCode == 65 || e.keyCode == 81) player.movement.LEFT = keyState;
+  if (e.keyCode == 37 || e.keyCode == 65 || e.keyCode == 81) game.player.movement.LEFT = keyState;
   // spacebar
-  // if (e.keyCode === 32)
+  if (e.keyCode === 32) console.log(game)
 }
 
-window.addEventListener('load', () => {
-  RAF = requestAnimationFrame(loop);
-});
 window.addEventListener('keydown', keyHandler);
 window.addEventListener('keyup', keyHandler);
